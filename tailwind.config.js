f8a4d52afb54fd5c207205522add22fb060dd109
Jen Stehlik <jen.stehlik@kallysoft.ch>
2019-06-27 12:19:18 +0200

    setup stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
+    fontFamily: {
+      body: ['Rubik', 'sans-serif']
+    },
     extend: {}
   },
   variants: {}
